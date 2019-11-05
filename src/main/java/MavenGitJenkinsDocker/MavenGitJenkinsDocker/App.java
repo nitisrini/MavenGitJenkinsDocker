24fc557bba277f0581ab208756052d78ed294709
Srinivasan K <Srinivasan.k@invicara.com>
2019-11-05 16:37:57 +0530

    Adding project

---
+++ src/main/java/MavenGitJenkinsDocker/MavenGitJenkinsDocker/App.java
@@ -0,0 +1,13 @@
+package MavenGitJenkinsDocker.MavenGitJenkinsDocker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Demo of MavenGitJenkinsDocker" );
+    }
+}
---
+++ src/test/java/MavenGitJenkinsDocker/MavenGitJenkinsDocker/NewTest.java
@@ -0,0 +1,11 @@
+package MavenGitJenkinsDocker.MavenGitJenkinsDocker;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+  public void f() {
+	  System.out.println("Test for Maven TestNG in docker");
+  }
+}
